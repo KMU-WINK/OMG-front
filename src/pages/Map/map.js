7c5d03789eb9a9950275cf92439cb35927632f64
Park Jungmin <65996166+kmujm@users.noreply.github.com>
2022-11-22 23:47:41 +0900

    feat: ✨ kakao map api 추가

--- src/pages/Map/map.js
+++ src/pages/Map/map.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import * as style from './styles';
+import React, { useEffect } from "react";
+import * as style from "./styles";
+
+import Footer from "../../components/Footer/footer";
+
+const { kakao } = window;
 
 function Map(props) {
+  useEffect(() => {
+    const mapScript = document.createElement("script");
 
-    return (
-        <style.Wrap>
-            Map Page
-        </style.Wrap>
-    );
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAOMAP_KEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      kakao.maps.load(() => {
+        const container = document.getElementById("map");
+        const options = {
+          center: new kakao.maps.LatLng(33.450701, 126.570667),
+        };
+        const map = new kakao.maps.Map(container, options);
+        const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+        const marker = new kakao.maps.Marker({
+          position: markerPosition,
+        });
+        marker.setMap(map);
+      });
+    };
+    mapScript.addEventListener("load", onLoadKakaoMap);
+
+    return () => mapScript.removeEventListener("load", onLoadKakaoMap);
+  }, []);
+
+  return (
+    <style.Wrap>
+      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
+      <Footer />
+    </style.Wrap>
+  );
 }
 
 export default Map;
