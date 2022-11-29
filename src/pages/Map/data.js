f4cc7666ebdffb9ca665441872dc9849c5fd8515
Park Jungmin <65996166+kmujm@users.noreply.github.com>
2022-11-30 00:13:39 +0900

    feat: map mock data

---
+++ src/pages/Map/data.js
@@ -0,0 +1,53 @@
+export const MARKERDATA = [
+  {
+    title: "카카오", // 등록_id
+    lat: 37.60983939384303,
+    lng: 126.99454107397042,
+  },
+  {
+    title: "생태연못",
+    lat: 37.63983939384303,
+    lng: 126.99454107397042,
+  },
+  {
+    title: "텃밭",
+    lat: 37.6193939384303,
+    lng: 126.99454107397042,
+  },
+];
+
+export const DATA = [
+  {
+    _id: "1alsfoisjdfoa",
+    sojuNum: 20,
+    beerNum: 1,
+    extraNum: 0,
+    money: 1000,
+    address: "서울 성북구 솔샘로 6길 16-16",
+    isLiked: false,
+    click: 35,
+    like: 10,
+  },
+  {
+    _id: "2bosadijfaosdf",
+    sojuNum: 2,
+    beerNum: 10,
+    extraNum: 1,
+    money: 1500,
+    address: "서울 성북구 솔샘로 6길 16-16",
+    isLiked: true,
+    click: 25,
+    like: 6,
+  },
+  {
+    _id: "3calsdfjaldskfja",
+    sojuNum: 20,
+    beerNum: 1,
+    extraNum: 0,
+    money: 1000,
+    address: "서울 성북구 솔샘로 6길 16-16",
+    isLiked: false,
+    click: 35,
+    like: 10,
+  },
+];
