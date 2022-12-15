c4dd27e05a32d820743db7dcc37b84b4eb27b93f
sanglim <sanglim@kookmin.ac.kr>
2022-12-16 00:25:16 +0900

    feat. 회원가입 창 제작중

--- src/pages/Join/styles.js
+++ src/pages/Join/styles.js
@@ -1,4 +1,23 @@
 import styled from 'styled-components';
 
 export const Wrap = styled.div`
+    margin-top: 50px;
+  
+  >div:last-child {
+    padding: 20px;
+  }
+`;
+export const InputWrap = styled.div`
+  > hr {
+    margin: 20px 0;
+    width: 100%;
+  }
+  > div:first-child {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  > div:last-child {
+    margin-bottom: 60px;
+  }
 `;
