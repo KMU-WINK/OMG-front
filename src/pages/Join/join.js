ee782fbc4d76e141ff6463727ac154befbbfbea8
sanglim <sanglim@kookmin.ac.kr>
2022-12-20 01:03:42 +0900

    feat. 체크박스 제작중

--- src/pages/Join/join.js
+++ src/pages/Join/join.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as style from './styles';
 import Header from "../../components/Header/header";
 import FullButton from "../../components/Button/fullButton";
 import Input from "../../components/Input/input";
+import {Checkbox, CheckBox} from "./styles";
 
 function Join(props) {
 
+    // 전체선택, privacy policy, terms of service
+    const [all, setAll] = useState(false);
+    const [pp, setPP] = useState(false);
+    const [ts, setTs] = useState(false);
+
+    const [check, setCheck] = useState("");
+
+    const checkBoxClicked = async (e) => {
+        // e.preventDefault();
+        if(e.target.tagName === "svg") {
+            setCheck(e.target.parentElement.id);
+        }
+        else if(e.target.tagName === "path") {
+            setCheck(e.target.parentElement.parentElement.id);
+        }
+        else {
+            setCheck(e.target.id);
+        }
+
+        if(check === "all" && all === true) {
+            setAll(false);
+            setPP(false);
+            setTs(false);
+        }
+        else if(check === "all" && all === false) {
+            setAll(true);
+            setPP(true);
+            setTs(true);
+        }
+        else if(check === "pp" && pp === true) setPP(false);
+        else if(check === "pp" && pp === false) setPP(true);
+        else if(check === "ts" && ts === true) setTs(false);
+        else if(check === "ts" && ts === false) setTs(true);
+
+    }
+
+
+    console.log("all: ", all, ", pp: ", pp, ", ts: ", ts)
     return (
         <style.Wrap>
             <Header title={"회원가입"} />
@@ -21,15 +60,15 @@ function Join(props) {
                     </div>
                     <div>
                         <div>
-                            <input type={"checkbox"} />
+                            <Checkbox checked={all} id={"all"} onClick={checkBoxClicked}/>
                             <span>전체 선택</span>
                         </div>
                         <div>
-                            <input type={"checkbox"} />
+                            <CheckBox checked={pp} id={"pp"} onClick={checkBoxClicked}/>
                             <span>개인정보 처리방침을 확인했습니다.</span>
                         </div>
                         <div>
-                            <input type={"checkbox"} />
+                            <CheckBox checked={ts} id={"ts"} onClick={checkBoxClicked}/>
                             <span>서비스 이용방침을 확인했습니다.</span>
                         </div>
                     </div>
--- src/pages/Join/styles.js
+++ src/pages/Join/styles.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
+import React from "react";
 
 export const Wrap = styled.div`
     margin-top: 50px;
   
-  >div:last-child {
+  > div:last-child {
     padding: 20px;
   }
 `;
@@ -19,5 +20,50 @@ export const InputWrap = styled.div`
   }
   > div:last-child {
     margin-bottom: 60px;
+    > div {
+      display: flex;
+      gap: 10px;
+      
+      :nth-child(2) {
+        margin: 20px 0 8px 0;
+      }
+    }
   }
 `;
+
+export const svgC = styled.svg`
+  fill: none;
+  stroke: ${(props) => (props.checked ? '#009800' : '#E0E0E0')}; ;
+  stroke-width: 2px;
+  width: 12px;
+  height: 11px;
+`;
+
+
+export const CheckBox = styled.div`
+  border: ${(props) => (props.checked ? '2px solid #009800' : '2px solid #E0E0E0')};
+  background-color: ${(props) => (props.checked ? '#009800' : '')};
+  border-radius: 4px;
+  width: 18px;
+  height: 18px;
+  text-align: center;
+`;
+
+export const S = styled.svg`
+  width:12px;
+  height:11px; 
+  fill:none;
+`;
+export const P = styled.path`
+  stroke: ${(props) => (props.checked ? '#ffffff' : '')};
+  stroke-width: 2px;
+`;
+
+export const Checkbox = ({ checked, onClick, id }) => (
+    <CheckBox type="button" onClick={onClick} id={id} checked={checked}>
+        <S>
+            <P d="M1.00005 6.81818L3.90914 9L10.4546 1" checked={checked}/>
+        </S>
+    </CheckBox>
+);
+
