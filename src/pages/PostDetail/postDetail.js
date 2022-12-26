5107fd5afbb7918a4a0777e6fc81c4a608497af7
dan-2ee <ekdms7273@naver.com>
2022-12-27 06:25:26 +0900

    feat: 게시글 내용 보여줌

--- src/pages/PostDetail/postDetail.js
+++ src/pages/PostDetail/postDetail.js
@@ -2,15 +2,38 @@ import React from "react";
 import * as style from "./styles";
 import Footer from "../../components/Footer/footer";
 import Header from "../../components/Header/header";
+import { click } from "@testing-library/user-event/dist/click";
 
 function PostDetail(props) {
     const clickMenu = () => {
         alert("메뉴 클릭")
     }
 
+    const clickReaction = () => {
+        alert("공감 클릭")
+    }
+
     return (
         <style.Wrap>
             <Header title={"게시글"} onClick={clickMenu}/>
+            <style.infoContainer>
+                <style.profileImg src={process.env.PUBLIC_URL + '/images/Common/Profile.svg'}/>
+                <style.userInfoBox>
+                    <span> {"이다은"} </span>
+                    <span> 수거 <span style={{color: "#009800"}}> 3회 </span> | 
+                    등록 <span style={{color: "#009800"}}> 2회 </span> | 
+                    공병지수 <span style={{color: "#009800"}}> 100ml </span> </span>
+                </style.userInfoBox>
+                <span style={{color: "#7A7171", marginLeft: "25px"}}>2시간 전</span>
+            </style.infoContainer>
+            <style.title >공병 회수에 대해 알아보자!</style.title>
+            <style.content>하나, 공병을 가지고 있으면 인근 매장에 가서 보증금을 받으세요 둘, 공병을 가지고 있으면 인근 매장에 가서 보증금을 받으세요. </style.content>
+            <style.reactionBox>
+                <style.reactionImg src={process.env.PUBLIC_URL + '/images/Board/Happy.svg'}/>
+                <span style={{color: "#009800", fontSize: "14px", alignSelf: "center", cursor: "pointer"}} onClick={clickReaction}> 공감하기 </span>
+                <style.reactionImg src={process.env.PUBLIC_URL + '/images/Board/Message.svg'}/>
+                <span style={{fontSize: "14px", alignSelf: "center", marginRight: "30px"}}> 5 </span>
+            </style.reactionBox>
         </style.Wrap>
     )
 }
--- src/pages/PostDetail/styles.js
+++ src/pages/PostDetail/styles.js
@@ -5,3 +5,67 @@ export const Wrap = styled.div`
     height: 100%;
     max-width: 480px;
 `;
+
+export const infoContainer = styled.div`
+    width: 100%;
+    height: fit-content;
+    display: flex;
+    flex-direction: row;
+    margin-top: 92px;
+    justify-content: center;
+`;
+
+export const profileImg = styled.img`
+    width: 36px;
+    height: 36px;
+`
+
+export const userInfoBox = styled.div`
+    width: fit-content;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    margin-left: 17px;
+`;
+
+export const title = styled.div`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    color: #000000;
+    margin-top: 38px;
+    margin-left: 10%;
+`;
+
+export const content = styled.div`
+    width: 80%;
+    height: fit-content;
+    min-height: 100px;
+    margin-top: 10px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    color: #000000;
+    margin-top: 25px;
+    margin-left: 10%;
+`;
+
+export const reactionBox = styled.div`
+    width: 100%;
+    max-width: 480px;
+    height: fit-content;
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 22px;
+
+    padding-bottom: 12px;
+    border-bottom: 1.5px solid #E9E9E9;
+    border-top: none; border-left: none; border-right: none;
+`;
+
+export const reactionImg = styled.img`
+    width: 24px;
+    height: 24px;
+    margin-right: 5px;
+    margin-left: 15px;
+`
