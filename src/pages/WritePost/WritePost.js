5b808039dd03d5f435687dff9a670e0a66b215c9
dan-2ee <ekdms7273@naver.com>
2022-12-27 04:16:08 +0900

    feat: 글 제목 및 내용 저장

--- src/pages/WritePost/WritePost.js
+++ src/pages/WritePost/WritePost.js
@@ -1,18 +1,29 @@
-import react from "react";
+import react, {useState} from "react";
 import * as style from "./styles";
 import Footer from "../../components/Footer/footer";
 import Header from "../../components/Header/header";
 
 function WritePost(props) {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
     const UploadPost = () => {
-        alert("upload clicked");
+        if (!title) alert("글 제목을 입력하세요")
+        if (!content) alert("글 내용을 입력하세요")
+    }
+
+    const changeValue = (name) => (e) => {
+        switch(name) {
+            case "title": setTitle(e.target.value); break;
+            case "content": setContent(e.target.value); break;
+            default: break;
+        }
     }
 
     return (
         <style.Wrap>
             <Header title={"글 작성하기"} onClick={UploadPost}/>
-            <style.titleInput type={"text"} placeholder={"제목"}/>
+            <style.titleInput type={"text"} placeholder={"제목"} onChange={changeValue("title")}/>
             <style.cardBox>
                 <style.noticeCard>
                     <span style={{fontWeight: "700"}}> {`이런 글은 작성하실 수 없어요. \n`} </span>
@@ -24,7 +35,7 @@ function WritePost(props) {
                     </span>
                 </style.noticeCard>
             </style.cardBox>    
-            <style.contentInput type={"text"} placeholder={"글 내용을 입력하세요."}/>
+            <style.contentInput type={"text"} placeholder={"글 내용을 입력하세요."} onChange={changeValue("content")}/>
             <Footer />
         </style.Wrap>
     )
