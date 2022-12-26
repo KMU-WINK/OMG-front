8b6f5a3d17468b19d1deb755ae883155ef21f359
dan-2ee <ekdms7273@naver.com>
2022-12-27 04:36:14 +0900

    feat: 팝업 삭제 기능 구현

--- src/pages/WritePost/WritePost.js
+++ src/pages/WritePost/WritePost.js
@@ -6,6 +6,7 @@ import Header from "../../components/Header/header";
 function WritePost(props) {
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+    const [showPopup, setShowPopup] = useState(true);
 
     const UploadPost = () => {
         if (!title) alert("글 제목을 입력하세요")
@@ -24,17 +25,24 @@ function WritePost(props) {
         <style.Wrap>
             <Header title={"글 작성하기"} onClick={UploadPost}/>
             <style.titleInput type={"text"} placeholder={"제목"} onChange={changeValue("title")}/>
-            <style.cardBox>
-                <style.noticeCard>
-                    <span style={{fontWeight: "700"}}> {`이런 글은 작성하실 수 없어요. \n`} </span>
-                    <span style={{fontWeight: "500"}}>
-                        {`· 타인의 권리를 침해하거나 불쾌감을 주는 글 
-                        · 범죄, 불법 행위 등 법령을 위반하는 글 
-                        · 광고/홍보 목적의 글 
-                        · 정치/사회에 대한 의견 및 가치관을 드러내는 글`}
-                    </span>
-                </style.noticeCard>
-            </style.cardBox>    
+            {showPopup ? 
+                <style.cardBox>
+                    <style.noticeCard>
+                        <style.line>
+                        <span style={{fontWeight: "700"}}> {`이런 글은 작성하실 수 없어요. \n`} </span>
+                        <button style={{background:"none", border: "none", marginLeft: "40%"}} onClick={() => setShowPopup(false)}>
+                            <img src={process.env.PUBLIC_URL + '/images/Common/close.svg'} />
+                        </button> 
+                        </style.line>
+                        <span style={{fontWeight: "500"}}>
+                            {`· 타인의 권리를 침해하거나 불쾌감을 주는 글 
+                            · 범죄, 불법 행위 등 법령을 위반하는 글 
+                            · 광고/홍보 목적의 글 
+                            · 정치/사회에 대한 의견 및 가치관을 드러내는 글`}
+                        </span>
+                    </style.noticeCard>
+            </style.cardBox> 
+        : null }
             <style.contentInput type={"text"} placeholder={"글 내용을 입력하세요."} onChange={changeValue("content")}/>
             <Footer />
         </style.Wrap>
