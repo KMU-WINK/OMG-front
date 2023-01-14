30749ceee1af4a19989ea44841bc4fd31c728925
sanglim <sanglim@kookmin.ac.kr>
2023-01-15 04:32:13 +0900

    style. board page css 수정

--- src/pages/Board/board.js
+++ src/pages/Board/board.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
-import * as style from "./styles";
 import { useNavigate } from "react-router-dom";
 import Footer from "../../components/Footer/footer";
 import Header from "../../components/Header/header";
 import Card from "../../components/Card/card";
 import { boardService } from "../../apis/services/board";
+import {
+  Wrap,
+  Contents,
+  WriteImg,
+  BoardContent,
+  Search,
+  MainTitle,
+} from "./styles";
 
-function Board(props) {
+function Board() {
   const navigator = useNavigate();
   const [boardList, setBoardList] = useState([]);
 
@@ -21,41 +28,42 @@ function Board(props) {
   }, []);
 
   return (
-    <style.Wrap>
-      <style.btnWrite
+    <Wrap>
+      <Header title={"게시판"} />
+      <Contents>
+        <div>
+          <Search type={"text"} placeholder={"단어를 검색해보세요"} />
+        </div>
+        <div>
+          <MainTitle>내 주변 소식</MainTitle>
+          <BoardContent>
+            {boardList.map((board) => (
+              <Card
+                id={board["id"]}
+                name={board["user"]["name"]}
+                bottleSell={board["user"]["bottleSell"]}
+                bottleBuy={board["user"]["bottleBuy"]}
+                point={board["user"]["point"]}
+                title={board["title"]}
+                contents={board["content"]}
+                likes={board["likes"].length}
+                comments={board["comments"].length}
+                userId={board["user"]["id"]}
+              />
+            ))}
+          </BoardContent>
+        </div>
+      </Contents>
+      <WriteImg
+        src={process.env.PUBLIC_URL + "/images/Board/Write.svg"}
         onClick={() =>
           navigator("/write-post", {
             state: { title: "", contents: "", type: "UPLOAD" },
           })
         }
-      >
-        <style.writeImg
-          src={process.env.PUBLIC_URL + "/images/Board/Write.svg"}
-        />
-      </style.btnWrite>
-      <Header title={"게시판"} />
-      <style.SearchContainer>
-        <style.Search type={"text"} placeholder={"단어를 검색해보세요"} />
-      </style.SearchContainer>
-      <style.mainTitle>내 주변 소식</style.mainTitle>
-      <style.boardContent>
-        {boardList.map((board) => (
-          <Card
-            id={board["id"]}
-            name={board["user"]["name"]}
-            bottleSell={board["user"]["bottleSell"]}
-            bottleBuy={board["user"]["bottleBuy"]}
-            point={board["user"]["point"]}
-            title={board["title"]}
-            contents={board["content"]}
-            likes={board["likes"].length}
-            comments={board["comments"].length}
-            userId={board["user"]["id"]}
-          ></Card>
-        ))}
-      </style.boardContent>
+      />
       <Footer />
-    </style.Wrap>
+    </Wrap>
   );
 }
 
