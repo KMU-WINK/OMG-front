bb51efe119dd976b5039ee80ff334da57276a404
Park Jungmin <65996166+kmujm@users.noreply.github.com>
2023-01-15 04:38:42 +0900

    fix: header backbtn 버그 수정

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,30 +1,41 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-import * as style from './styles';
+import React from "react";
+import { useNavigate } from "react-router";
+import * as style from "./styles";
 
 function Header(props) {
+  const navigator = useNavigate();
 
-    const navigator = useNavigate();
-
-    return (
-        <style.Wrap>
-            <style.Title>
-                <img src={process.env.PUBLIC_URL + '/images/Header/backIcon.svg'}/>
-                <span>{props.title}</span>
-                {
-                    props.title === '마이페이지' ?
-                        <img src={process.env.PUBLIC_URL + '/images/Header/settingIcon.svg'} onClick={()=> navigator('/setting')}/>
-                    : props.title === "글 작성하기" ?
-                        <button onClick={props.onClick}> 확인 </button>
-                    : props.title === "게시글" ?
-                        <img src={process.env.PUBLIC_URL + '/images/Common/meatballs.svg'} onClick={props.onClick}/>
-                    : props.title === "알림" ?
-                        <img src={process.env.PUBLIC_URL + '/images/Common/meatballs.svg'} onClick={props.onClick}/>
-                    :   null
-                }
-            </style.Title>
-        </style.Wrap>
-    );
+  return (
+    <style.Wrap>
+      <style.Title>
+        <img
+          src={process.env.PUBLIC_URL + "/images/Header/backIcon.svg"}
+          onClick={() => {
+            navigator(-1);
+          }}
+        />
+        <span>{props.title}</span>
+        {props.title === "마이페이지" ? (
+          <img
+            src={process.env.PUBLIC_URL + "/images/Header/settingIcon.svg"}
+            onClick={() => navigator("/setting")}
+          />
+        ) : props.title === "글 작성하기" ? (
+          <button onClick={props.onClick}> 확인 </button>
+        ) : props.title === "게시글" ? (
+          <img
+            src={process.env.PUBLIC_URL + "/images/Common/meatballs.svg"}
+            onClick={props.onClick}
+          />
+        ) : props.title === "알림" ? (
+          <img
+            src={process.env.PUBLIC_URL + "/images/Common/meatballs.svg"}
+            onClick={props.onClick}
+          />
+        ) : null}
+      </style.Title>
+    </style.Wrap>
+  );
 }
 
 export default Header;
