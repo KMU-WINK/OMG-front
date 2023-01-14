70d41e7d12ad0169dafbd639c12a7f4ffdd80147
dan-2ee <ekdms7273@naver.com>
2023-01-14 22:57:46 +0900

    feat: 게시판 글 상세 페이지  api 연동

--- src/pages/PostDetail/postDetail.js
+++ src/pages/PostDetail/postDetail.js
@@ -91,14 +91,15 @@ function PostDetail(props) {
                 <style.profileImg src={process.env.PUBLIC_URL + '/images/Common/Profile.svg'}/>
                 <style.userInfoBox>
                     <span> {state.name} </span>
-                    <span> 수거 <span style={{color: "#009800", fontWeight: "700"}}> {state.getCnt}회 </span> | 
-                    등록 <span style={{color: "#009800", fontWeight: "700"}}> {state.setCnt}회 </span> | 
+                    <span> 수거 <span style={{color: "#009800", fontWeight: "700"}}> {state.bottleBuy}회 </span> | 
+                    등록 <span style={{color: "#009800", fontWeight: "700"}}> {state.bottleSell}회 </span> | 
                     공병지수 <span style={{color: "#009800", fontWeight: "700"}}> {state.point}ml </span> </span>
                 </style.userInfoBox>
                 <span style={{color: "#7A7171", marginLeft: "25px"}}>2시간 전</span>
             </style.infoContainer>
-            <style.title >공병 회수에 대해 알아보자!</style.title>
-            <style.content>하나, 공병을 가지고 있으면 인근 매장에 가서 보증금을 받으세요 둘, 공병을 가지고 있으면 인근 매장에 가서 보증금을 받으세요. </style.content>
+            <style.title >{state.title}</style.title>
+            <style.content>{state.contents} </style.content>
+
             <style.reactionBox>
                 <style.reactionImg src={process.env.PUBLIC_URL + '/images/Board/Happy.svg'}/>
                 <span style={{color: "#009800", fontSize: "14px", alignSelf: "center", cursor: "pointer"}} onClick={openReationModal}> 공감하기 </span>
@@ -107,10 +108,13 @@ function PostDetail(props) {
             button1={closeReactionModal} button1Content="확인">
                 <span style={{fontSize: "18px", fontWeight: "bold", alignSelf: "center"}}>{"이 글을 공감하시겠습니까?"}</span>
                 </Modal4> : null}
+
                 <style.reactionImg src={process.env.PUBLIC_URL + '/images/Board/Message.svg'}/>
-                <span style={{fontSize: "14px", alignSelf: "center", marginRight: "30px"}}> 5 </span>
+                <span style={{fontSize: "14px", alignSelf: "center", marginRight: "30px"}}> {state.comments} </span>
             </style.reactionBox>
+
             <style.underLine />
+            
             <style.SearchContainer>
                 <input type={"text"} placeholder={"댓글을 입력해주세요"}/>
                 <img src={process.env.PUBLIC_URL + '/images/Board/SendComment.svg'} onClick={clickPost}/>
