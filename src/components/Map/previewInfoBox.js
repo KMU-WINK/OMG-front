6cf832a2f82f780f890d5d36bbcedead07c061b3
Park Jungmin <65996166+kmujm@users.noreply.github.com>
2023-01-15 07:13:41 +0900

    fix: map page bottle 조회, 관심 버그 수정

--- src/components/Map/previewInfoBox.js
+++ src/components/Map/previewInfoBox.js
@@ -7,15 +7,19 @@ import { bottleService } from "../../apis/services/bottle";
 function PreviewInfoBox(props) {
   const navigator = useNavigate();
   const [like, setLike] = useState(props.isLiked);
+  const [data, setData] = useState(props);
 
   const handleLikeClick = (e) => {
     //_id로 isLiked 갱신
     if (like) {
       setLike(false);
       bottleService.deleteLike(props._id);
+      setData({ ...data, likeNum: data.likeNum - 1 });
+      console.log(data);
     } else {
       setLike(true);
       bottleService.addLike(props._id);
+      setData({ ...data, likeNum: data.likeNum + 1 });
     }
   };
 
@@ -42,7 +46,7 @@ function PreviewInfoBox(props) {
         <style.Money>보증금 {props.money}원</style.Money> 예상
         <style.Address>{props.address}</style.Address>
       </style.SubContent>
-      <style.ExtraContent>{`조희 ${props.clickNum} | 관심 ${props.likeNum}`}</style.ExtraContent>
+      <style.ExtraContent>{`조희 ${props.clickNum} | 관심 ${data.likeNum}`}</style.ExtraContent>
       <style.DetailBtn
         onClick={async () => {
           // 공병 상세 페이지로 이동
